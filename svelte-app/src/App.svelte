c0561c1faf20ff2fd4d504aca208258c82cbe1b9
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2021-12-27 22:45:15 -0500

    Merging script-injection

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,38 +1,4 @@
 <script>
-<<<<<<< HEAD
-import {snapshots, fileTree} from './stores.js';
-import Component from './Component.svelte';
-import TidyTree from './TidyTree.svelte';
-
-
-$: snapshot = $snapshots[CurrentI];
-let CurrentI;
-
-$: view = selection;
-let selection;
-
-function selectState(index) {
-	CurrentI = index;
-}
-
-function selectView(view) {
-	selection = view;
-}
-
-</script>
-
-<main>
-	<p>Svelte Slicer</p>
-	<button on:click={() => selectView("componentTree")}>Component Tree</button><button on:click={() => selectView("state")}>State</button>
-	<hr>
-	{#if view === "componentTree"} 
-		<Component component={$fileTree}/>
-		<TidyTree treeData={$fileTree}/>
-	{:else if view === "state"}
-		{#each $snapshots as snapshot, i}
-			<button on:click={() => selectState(i)}>Snapshot {i}</button>
-		{/each}
-=======
 	import {snapshots, fileTree} from './stores.js';
 	import Component from './Component.svelte';
 	import TidyTree from './TidyTree.svelte';
@@ -58,7 +24,6 @@ function selectView(view) {
 	<main>
 		<p>Svelte Slicer</p>
 		<button on:click={() => selectView("componentTree")}>Component Tree</button><button on:click={() => selectView("state")}>State</button>
->>>>>>> script-injection
 		<hr>
 		{#if view === "componentTree"} 
 			<Component component={$fileTree}/>
--- svelte-app/src/Component.svelte
+++ svelte-app/src/Component.svelte
@@ -1,13 +1,8 @@
 <script>
     export let component;
-<<<<<<< HEAD
     
     console.log("component.children",component.children);
     console.log('hierarchical data for tidy tree',component)
-=======
-
-    console.log(component);
->>>>>>> script-injection
 </script>
 
 <main>
