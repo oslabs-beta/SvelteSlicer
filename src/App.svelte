6f02a9b66b20f7106fd58e4ba881d4cfc86ad35e
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2021-12-27 21:57:00 -0500

    Adding TidyTree to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,52 +1,57 @@
 <script>
-import {snapshots, fileTree} from './stores.js';
-import ComponentTree from './ComponentTree.svelte';
-import Component from './Component.svelte';
-
-$: snapshot = $snapshots[CurrentI];
-let CurrentI;
-
-$: view = selection;
-let selection;
-
-function selectState(index) {
-	CurrentI = index;
-}
-
-function selectView(view) {
-	selection = view;
-}
-
-</script>
-
-<main>
-	<h2>Svelte Slicer</h2>
-	<button on:click={() => selectView("componentTree")}>Component Tree</button><button on:click={() => selectView("state")}>State</button>
-	<hr>
-	{#if view === "componentTree"} 
-		<ComponentTree testing={$fileTree}/>
-	{:else if view === "state"}
-		{#each $snapshots as snapshot, i}
-			<button on:click={() => selectState(i)}>Snapshot {i}</button>
-		{/each}
+	import {snapshots, fileTree} from './stores.js';
+	import Component from './Component.svelte';
+	import TidyTree from './TidyTree.svelte';
+	
+	
+	$: snapshot = $snapshots[CurrentI];
+	let CurrentI;
+	
+	$: view = selection;
+	let selection;
+	
+	function selectState(index) {
+		CurrentI = index;
+	}
+	
+	function selectView(view) {
+		selection = view;
+	}
+	
+	</script>
+	
+	<main>
+		<p>Svelte Slicer</p>
+		<button on:click={() => selectView("componentTree")}>Component Tree</button><button on:click={() => selectView("state")}>State</button>
 		<hr>
-		{#if snapshot} 
-			<Component component={snapshot}/>
+		{#if view === "componentTree"} 
+			<Component component={$fileTree}/>
+			<TidyTree treeData={$fileTree}/>
+		{:else if view === "state"}
+			{#each $snapshots as snapshot, i}
+				<button on:click={() => selectState(i)}>Snapshot {i}</button>
+			{/each}
+			<hr>
+			{#if snapshot} 
+				<Component component={snapshot}></Component>
+			{/if}
 		{/if}
-	{/if}
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
+	
+		
+	</main>
+	
+	<style>
 		main {
-			max-width: none;
+			/* text-align: center; */
+			padding: 1em;
+			max-width: 240px;
+			margin: 0 auto;
 		}
-	}
-</style>
+	
+		@media (min-width: 640px) {
+			main {
+				max-width: none;
+			}
+		}
+	</style>
+	
