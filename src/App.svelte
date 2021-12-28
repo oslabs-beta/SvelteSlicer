cd7072b58302090896ca4edef8bd365dcab6f71f
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2021-12-27 22:52:20 -0500

    Fix error from merge

--- src/App.svelte
+++ src/App.svelte
@@ -37,28 +37,11 @@
 				<State component={snapshot}></State>
 			{/if}
 		{/if}
-<<<<<<< HEAD
-	{/if}
-
-	
-</main>
-
-<style>
-	main {
-		/* text-align: center; */
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	@media (min-width: 640px) {
-=======
 	
 		
 	</main>
 	
 	<style>
->>>>>>> script-injection
 		main {
 			/* text-align: center; */
 			padding: 1em;
