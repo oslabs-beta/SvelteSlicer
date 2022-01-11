3437260fa2f2c334efa079cca5e34537746abef1
Rachel Collins <spiritedndiligent@gmail.com>
2022-01-11 16:59:32 -0500

    template for header and tab compoenents

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
 	import {snapshots, fileTree} from './stores.js';
 	import Component from './Component.svelte';
+	import Header from './Header.svelte';
+	import Tabs from './Header.svelte';
 	//import TidyTree from './TidyTree.svelte';
 	import TidyTree2 from './TidyTree2.svelte';
 	import State from './State.svelte';
 
 	let count=0;//control tidt tree render time on the dom. set render condition in TidyTree
-
+	let tabLinks = ['Home', 'Visual', 'Data'];
+	let clickedTab = 'Home'
 	$: snapshot = $snapshots[CurrentI];
 	$: data = (snapshot ? snapshot.data : undefined);
 
@@ -37,7 +40,8 @@
 	<main id="parent" style="display:flex; height:auto; box-sizing:content-box align-item:center">
 		<!-- <div id="left" class="center" style="background-color:#2D3436; height:100%; width:100%; border:solid 3px #F1F3F4; flex:{showLeft?3:0}"> -->
 			<div id="left" class="center" style=" border:solid 3px #F1F3F4; height:100%; width:100%; align-items; flex:{showLeft?3:0}">
-			<h2>Svelte Slicer</h2>
+			<Header/>
+				<h2>Svelte Slicer</h2>
 			<button on:click={() => selectView("componentTree")}>Tree</button><button on:click={() => selectView("state")}>State</button><button id="tidy" on:click={()=>selectTree("tidyTree")}>Chart</button>
 			<hr>
 			<!-- <label style="color:#F1F3F4; text-align:center"> -->
@@ -89,7 +93,8 @@
 			padding: 1em;
 			/* max-width: 240px; */
 			max-width:max-content;
-			margin: 0 auto;
+			margin: 20px auto;
+			align-items: center;
 		}
 	
 		@media (min-width: 640px) {
@@ -102,11 +107,12 @@
   position: relative;
   /* display: inline-block; */
   display: inline-flexbox;
-  width: 40px;
-  height: 16px;
+  width: 47px;
+  height: 22px;
 }
 
 .slider {
+  /* position: absolute; */
   position: absolute;
   cursor: pointer;
   top: 0;
@@ -115,6 +121,9 @@
   bottom: 0;
   background-color: #ccc;
   -webkit-transition: .4s;
+  -webkit-tap-highlight-color: transparent;
+  outline:#2196F3;
+
   transition: .4s;
 }
 
@@ -122,17 +131,17 @@
   position: absolute;
   /* content: "visual/data"; */
   content: "";
-  height: 13px;
-  width: 13px;
-  left: 4px;
-  bottom: 4px;
+  height: 20px;
+  width: 20px;
+  left: 5px;
+  bottom: 2px;
   background-color: white;
   -webkit-transition: .4s;
   transition: .4s;
 }
 
 .slider.round {
-  border-radius: 17px;
+  border-radius: 30px;
 }
 
 .slider.round:before {
@@ -145,13 +154,13 @@ input:checked + .slider {
 }
 
 input:focus + .slider {
-  box-shadow: 0 0 1px #2196F3;
+  box-shadow: 0 0 2px #2196F3;
 }
 
 input:checked + .slider:before {
-  -webkit-transform: translateX(26px);
-  -ms-transform: translateX(26px);
-  transform: translateX(26px);
+  -webkit-transform: translateX(23px);
+  -ms-transform: translateX(23px);
+  transform: translateX(23px);
 }
 	</style>
 	
---
+++ src/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+export let logo = './images/svelte_logo.png';
+</script>
+    <h2> <img src={logo} style='width:10%;' align='left' alt='slicer logo'/> Svelte Slicer</h2>
+<header>
+
+</header>
+
+<style>
+
+</style>
