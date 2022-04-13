80400757098673756f3137eea4cf7aa47d2e86f6
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-04-13 10:47:39 -0400

    Add Slicer class

---
+++ extension/devtools/injected2.js
@@ -0,0 +1,44 @@
+class Slicer {
+    constructor(window) {
+        this.slicer = (() => {
+            const variables = {
+                components: [],
+                deletedNodes: [],
+                insertedNodes: [],
+            }
+        
+            return {
+                get: (variableName) => variables[variableName],
+                getValue: (variableName, key) => variables[variableName][key],
+                has: (variableName, key) => variables[variableName].hasOwnProperty(key),
+                add: (variableName, value) => variables[variableName].push(value),
+                reset: (variableName) => variables[variableName].splice(0, variables[variableName].length),
+                update: (variableName, newValue, key) => variables[variableName][key] = newValue,
+                delete: (variableName, key) => delete variables[variableName][key]
+            };
+        })();
+
+        this.setup(window);
+    }
+
+    addSvelteDomListeners(root) {
+        root.addEventListener('SvelteRegisterComponent', this.registerNewComponent);
+    }
+
+    setup(window) {
+        this.addSvelteDomListeners(window.document);     
+        for (let i = 0; i < window.frames.length; i++) {
+            const frame = window.frames[i];
+            const root = frame.document;
+            this.addSvelteDomListeners(root);
+        }
+    }
+
+    registerNewComponent(e) {
+        console.log(e.detail);
+    }
+}
+
+new Slicer(window);
+
+module.exports = Slicer;
