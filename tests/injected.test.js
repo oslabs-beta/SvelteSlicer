c53b4120819d9ce4787ef3143f3bf0784a876531
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-04-13 16:22:39 -0400

    Add travis.yml file

--- tests/injected.test.js
+++ tests/injected.test.js
@@ -1,3 +1,7 @@
+/**
+ * @jest-environment jsdom
+ */
+
 const rewire = require('rewire');
 const injected = rewire('../extension/devtools/injected2.js');
 
@@ -7,5 +11,18 @@ describe('getComponentName', () => {
         const expectedResult = 'App';
         const getComponentName = injected.__get__('getComponentName');
         expect(getComponentName(filePath)).toBe(expectedResult);
+    })
+})
+
+describe('addSvelteDomListeners', () => {
+    test('it should call correct function when given custom event is fired', () => {
+        const addSvelteDomListeners = injected.__get__('addSvelteDomListeners');
+        const registerNewComponentMock = jest.fn();
+        injected.__set__('registerNewComponent', registerNewComponentMock);
+        addSvelteDomListeners(window.document);
+        // dispatch click event to listener
+        const addEvt = new Event('SvelteRegisterComponent');
+        document.dispatchEvent(addEvt);
+        expect(registerNewComponentMock).toHaveBeenCalledTimes(1);
     })
 })
