8c129e53b48bf7743b32240c763e222e124d2c02
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-04-21 22:05:39 -0400

    Update comments

--- extension/devtools/background.js
+++ extension/devtools/background.js
@@ -2,47 +2,46 @@ var connections = {};
 
 // Set up listeniing connection with devtool
 chrome.runtime.onConnect.addListener(function (port) {
+  // Listen to messages sent from the DevTools page
+  port.onMessage.addListener((message) => {
+    // Store tabID from initial message from devtool
+    if (message.name == "init") {
+      connections[message.tabId] = port;
+      return;
+    }
 
-    // Listen to messages sent from the DevTools page
-    port.onMessage.addListener((message) => {
-      // Store tabID from initial message from devtool
-      if (message.name == "init") {
-        connections[message.tabId] = port;
-        return;
+    // Forward messages from devtool on to the content script
+    if (message.name === "jumpState" || message.name === "clearSnapshots") {
+      chrome.tabs.sendMessage(message.tabId, message);
+    }
+  });
+
+  port.onDisconnect.addListener(function (port) {
+    port.onMessage.removeListener(extensionListener);
+
+    var tabs = Object.keys(connections);
+    for (var i = 0, len = tabs.length; i < len; i++) {
+      if (connections[tabs[i]] == port) {
+        delete connections[tabs[i]];
+        break;
       }
-      
-      // Forward messages from devtool on to the content script
-      if (message.name === "jumpState" || message.name === "clearSnapshots") {
-        chrome.tabs.sendMessage(message.tabId, message);
-      }
-    });
-
-    port.onDisconnect.addListener(function(port) {
-        port.onMessage.removeListener(extensionListener);
-
-        var tabs = Object.keys(connections);
-        for (var i=0, len=tabs.length; i < len; i++) {
-          if (connections[tabs[i]] == port) {
-            delete connections[tabs[i]]
-            break;
-          }
-        }
-    });
+    }
+  });
 });
 
 // Receive messages from content script and relay to the devtool
-chrome.runtime.onMessage.addListener(function(message, sender) {
-    // Messages from content scripts should have sender.tab set
-    if (sender.tab) {
-      var tabId = sender.tab.id;
-      // If valid message, forward to devtool
-      if (tabId in connections) {
-        connections[tabId].postMessage(message);
-      } else {
-        console.log("Tab not found in connection list.");
-      }
+chrome.runtime.onMessage.addListener(function (message, sender) {
+  // Messages from content scripts should have sender.tab set
+  if (sender.tab) {
+    var tabId = sender.tab.id;
+    // If valid message, forward to devtool
+    if (tabId in connections) {
+      connections[tabId].postMessage(message);
     } else {
-      console.log("sender.tab not defined.");
+      console.log("Tab not found in connection list.");
     }
-    return true;
+  } else {
+    console.log("sender.tab not defined.");
+  }
+  return true;
 });
