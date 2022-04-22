197e097cee46e53814fd7a28885e5dc86fa28d17
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-04-21 22:12:56 -0400

    Update comments

--- extension/devtools/listener.js
+++ extension/devtools/listener.js
@@ -1,12 +1,16 @@
-window.addEventListener('message', function(event) {
+// listen for messages from user context and pass to background script
+window.addEventListener("message", function (event) {
   // Only accept messages from the same frame
   if (event.source !== window) {
     return;
   }
 
   // Only accept messages that we know are ours
-  if (typeof event.data !== 'object' || event.data === null ||
-      !event.data.source === 'panel.js') {
+  if (
+    typeof event.data !== "object" ||
+    event.data === null ||
+    !event.data.source === "panel.js"
+  ) {
     return;
   }
 
@@ -15,25 +19,23 @@ window.addEventListener('message', function(event) {
   chrome.runtime.sendMessage(JSON.stringify(data));
 });
 
-chrome.runtime.onMessage.addListener(message => {
+// listen for messages from background script and pass to user context
+chrome.runtime.onMessage.addListener((message) => {
   if (message.name === "jumpState") {
     window.postMessage({
-      source: 'listener.js',
-      type: 'jumpState',
+      source: "listener.js",
+      type: "jumpState",
       index: message.index,
       state: message.state,
-      tree: message.tree
+      tree: message.tree,
     });
-  }
-
-  else if (message.name === "clearSnapshots") {
+  } else if (message.name === "clearSnapshots") {
     window.postMessage({
-      source: 'listeneter.js',
-      type: 'clearSnapshots',
+      source: "listeneter.js",
+      type: "clearSnapshots",
       index: message.index,
       clearType: message.clearType,
-      path: message.path
-    })
+      path: message.path,
+    });
   }
 });
-
