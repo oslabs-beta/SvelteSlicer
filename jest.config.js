d7763bc34f7bf5be544ec0ca7a974d2a59a49b35
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-12-29 16:45:26 -0500

    test: update jest config to transpile js and svelte files

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,18 @@
 /** @type {import('jest').Config} */
 const config = {
   verbose: true,
-  transform: {},
+  transform: {
+    "^.+\\.js$": "babel-jest",
+    "^.+\\.svelte$": [
+      "svelte-jester",
+      {
+        compilerOptions: {
+          dev: true,
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ["js", "svelte"],
 };
 
 export default config;
