01ca33877c6ca225c8e55d12a1537ae630884810
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-12-29 17:45:44 -0500

    test: add integration tests for RegisterComponent events

--- extension/devtools/tests/SvelteEventParser.test.js
+++ extension/devtools/tests/SvelteEventParser.test.js
@@ -1,35 +1,64 @@
 /**
- * @jest-environment jsdom
+ * @happy-dom/jest-environment
  */
 /*eslint-env jest */
-import SvelteEventParser from "../src/SvelteEventParser.js";
+import TestAppContext from "../utils/TestAppContext.js"; // class that creates a context for running app where devtool internals are accessible
+import { testData } from "../utils/testData.js"; // mapping of mock app entrypoints with expected values for different data points
+import { SvelteComponent } from "svelte";
+import { describe, expect } from "@jest/globals";
 
-describe("assignInstanceNumber", () => {
-  const parser = new SvelteEventParser();
-  test("it returns correct instance number based on component count", () => {
-    const componentName1 = "App";
-    const componentName2 = "Test";
-    expect(parser.assignInstanceNumber(componentName1)).toBe(1);
-    expect(parser.assignInstanceNumber(componentName1)).toBe(2);
-    expect(parser.assignInstanceNumber(componentName2)).toBe(1);
-    expect(parser.assignInstanceNumber(componentName2)).toBe(2);
-  });
-});
+// initialize the testing context
+const context = new TestAppContext();
 
-describe("assignComponentId", () => {
-  const parser = new SvelteEventParser();
-  test("it returns correct id string based on tagName and component count", () => {
-    const componentName1 = "App";
-    const componentName2 = "Test";
-    expect(parser.assignComponentId(componentName1)).toBe("App1");
-    expect(parser.assignComponentId(componentName2)).toBe("Test1");
-    expect(parser.assignComponentId(componentName1)).toBe("App2");
-    expect(parser.assignComponentId(componentName2)).toBe("Test2");
+// runs the full test suite for each mock app in the testData file
+describe.each(testData)("Test $name", (testApp) => {
+  const expected = testApp.data;
+  let router;
+
+  // get each mock app set up for testing
+  beforeAll(async () => {
+    // create the devtool backend and app in the testing context
+    await context.testSetUp(testApp.app);
+    // once context is prepped for this app, need to get router instance to outer scope to be accessible by tests
+    router = context.router;
+    return;
   });
-  test("it correctly updates sequence of added components", () => {
-    expect(parser.newComponents[0]).toBe("App1");
-    expect(parser.newComponents[1]).toBe("Test1");
-    expect(parser.newComponents[2]).toBe("App2");
-    expect(parser.newComponents[3]).toBe("Test2");
+
+  // close mock app to prepare for next set of tests
+  afterAll(() => {
+    context.cleanUp();
+  });
+
+  describe("Handle RegisterComponent events", () => {
+    const { componentRepresentations, componentInstances } = router.dataStore;
+    it("Adds new component representations to componentRepresentations map", () => {
+      const components = Object.keys(componentRepresentations);
+      const representation = componentRepresentations[components[0]];
+      const { totalComponents, componentIds } = expected;
+      // expect correct number of components to be stored
+      expect(components.length).toBe(totalComponents);
+      // expect correct componentId's
+      expect(components).toEqual(expect.arrayContaining(componentIds));
+      // expect correct data structure for representations
+      expect(representation).toEqual(
+        expect.objectContaining({
+          id: expect.any(String),
+          tagName: expect.any(String),
+          children: expect.arrayContaining([]),
+        })
+      );
+    });
+
+    it("Adds new component instances to componentInstances map", () => {
+      const components = Object.keys(componentInstances);
+      const instance = componentInstances[components[0]];
+      const { totalComponents, componentIds } = expected;
+      // expect correct number of components to be stored
+      expect(components.length).toBe(totalComponents);
+      // expect correct component Id's
+      expect(components).toEqual(expect.arrayContaining(componentIds));
+      // expect stored values to be SvelteComponenents
+      expect(instance).toBeInstanceOf(SvelteComponent);
+    });
   });
 });
