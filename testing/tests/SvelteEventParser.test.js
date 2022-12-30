6be9272a03c409283fb01d8b1f1da9405da367dc
Heather Barney <heatherbarney@Heathers-MacBook-Air.local>
2022-12-29 22:15:40 -0500

    test: bug fixes in test to allow beforeAll to run

--- testing/tests/SvelteEventParser.test.js
+++ testing/tests/SvelteEventParser.test.js
@@ -2,44 +2,36 @@
  * @happy-dom/jest-environment
  */
 /*eslint-env jest */
-import TestAppContext from "../utils/TestAppContext.js"; // class that creates a context for running app where devtool internals are accessible
-import { testData } from "../utils/testData.js"; // mapping of mock app entrypoints with expected values for different data points
-import { SvelteComponent } from "svelte";
+import TestAppContext from "../utils/TestAppContext.js";
 import { describe, expect } from "@jest/globals";
+import { testData } from "../mocks/testData.js";
+import { SvelteComponent } from "svelte";
 
-// initialize the testing context
 const context = new TestAppContext();
 
-// runs the full test suite for each mock app in the testData file
 describe.each(testData)("Test $name", (testApp) => {
-  const expected = testApp.data;
-  let router;
+  let expected;
+  let dataStore;
 
-  // get each mock app set up for testing
   beforeAll(async () => {
-    // create the devtool backend and app in the testing context
-    await context.testSetUp(testApp.app);
-    // once context is prepped for this app, need to get router instance to outer scope to be accessible by tests
-    router = context.router;
+    const { app, data } = testApp;
+    await context.testSetUp(app);
+    expected = data;
+    dataStore = context.router.dataStore;
     return;
   });
 
-  // close mock app to prepare for next set of tests
   afterAll(() => {
     context.cleanUp();
   });
 
   describe("Handle RegisterComponent events", () => {
-    const { componentRepresentations, componentInstances } = router.dataStore;
     it("Adds new component representations to componentRepresentations map", () => {
-      const components = Object.keys(componentRepresentations);
-      const representation = componentRepresentations[components[0]];
+      const components = Object.keys(dataStore.componentRepresentations);
+      const representation = dataStore.componentRepresentations[components[0]];
       const { totalComponents, componentIds } = expected;
-      // expect correct number of components to be stored
       expect(components.length).toBe(totalComponents);
-      // expect correct componentId's
       expect(components).toEqual(expect.arrayContaining(componentIds));
-      // expect correct data structure for representations
       expect(representation).toEqual(
         expect.objectContaining({
           id: expect.any(String),
@@ -50,14 +42,11 @@ describe.each(testData)("Test $name", (testApp) => {
     });
 
     it("Adds new component instances to componentInstances map", () => {
-      const components = Object.keys(componentInstances);
-      const instance = componentInstances[components[0]];
+      const components = Object.keys(dataStore.componentInstances);
+      const instance = dataStore.componentInstances[components[0]];
       const { totalComponents, componentIds } = expected;
-      // expect correct number of components to be stored
       expect(components.length).toBe(totalComponents);
-      // expect correct component Id's
       expect(components).toEqual(expect.arrayContaining(componentIds));
-      // expect stored values to be SvelteComponenents
       expect(instance).toBeInstanceOf(SvelteComponent);
     });
   });
--- testing/utils/TestAppContext.js
+++ testing/utils/TestAppContext.js
@@ -11,27 +11,22 @@ export default class TestAppContext {
    * Creates a TestAppContext object
    */
   constructor() {
-    this.app = null; // store app instance for running app
-    this.router = null; // store router instance for running app
-    this.registrator = GlobalRegistrator; // store happy-dom's GlobalRegistrator function
+    this.app = null;
+    this.router = null;
+    this.registrator = GlobalRegistrator;
   }
 
-  /**
-   * Creates a happy-dom environment and creates and stores running instances of the mock app and the devtool backend within it.
-   * @param { SvelteComponent } appInstance // The mock app to be tested.
-   * @returns
-   */
   async testSetUp(appInstance) {
-    // create a happy-dom GlobalWindow and register it so that context is now mirrored globally
+    // create a happy-dom instance and registers it globally
     this.registrator.register();
 
-    // use exported function from injected script test file to create the Router instance and store it
+    // create the router instance and store it
     this.router = init();
 
     // create the app instance and store it
     this.app = new appInstance({ target: document.body });
 
-    // wait for page load to complete before exiting setup so Router receives events for initial load before tests are run
+    // wait for the first page load to complete before exiting setup
     await new Promise((resolve) => {
       window.addEventListener("load", () => {
         resolve();
@@ -41,9 +36,6 @@ export default class TestAppContext {
     return;
   }
 
-  /**
-   * Undoes the global registration of the GlobalWindow, allowing for another GlobalWindow to be registered.
-   */
   cleanUp() {
     this.registrator.unregister();
   }
